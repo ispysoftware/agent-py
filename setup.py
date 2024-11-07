a923e4b1ae082350dc0973ed88950d078d8cfb55
sean t <sean@ispyconnect.com>
2024-11-07 10:15:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.md').read()
 
 setup(
     name='agent-py',
-    version='0.0.23',
+    version='0.0.24',
     license='Apache Software License',
     url='https://github.com/ispysoftware/agent-py',
     author='Sean Tearney',
